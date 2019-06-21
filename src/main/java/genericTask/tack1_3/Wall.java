89ac34c5a345cb7efd1be1edc85e939ce5a33853
QAEng2827 <inna.nakopyuk@gmail.com>
2019-06-21 23:40:45 +0300

    HW6 Generic(task1_1, task1_2) +gitignore +task1_3 +task2_1

--- src/main/java/genericTask/tack1_3/Wall.java
+++ src/main/java/genericTask/tack1_3/Wall.java
@@ -5,6 +5,7 @@ class Wall extends HouseSurface {
     void paint(){
         System.out.println("We paint wall in different collors");
     }
+
     public void color(){
 
         System.out.println("White-and-Black");
